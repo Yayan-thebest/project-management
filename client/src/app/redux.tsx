b896b7216d412c5a112ab5db227e22d5ee43cb72
Yayan-thebest <monedgard@yahoo.com>
2025-02-23 16:43:33 -0500

    Fix lint errors

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -1,83 +1,82 @@
 import React from "react";
 import { useRef } from "react";
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import {
-  TypedUseSelectorHook,
-  useDispatch,
-  useSelector,
-  Provider,
-} from "react-redux";
-import globalReducer from "@/state";
-import { api } from "@/state/api";
 import { setupListeners } from "@reduxjs/toolkit/query";
-
-import {
-  persistStore,
-
-} from "redux-persist";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE, Storage, WebStorage } from 'redux-persist';
+import { api } from "@/state/api";
+import globalReducer from "@/state";
+import createWebStorage from "redux-persist/es/storage/createWebStorage";
 import { PersistGate } from "redux-persist/integration/react";
 
 /* REDUX PERSISTENCE */
-{/*const createNoopStorage = () => {
+const createNoopStorage = (): Storage => {
   return {
     getItem() {
       return Promise.resolve(null);
     },
-    setItem(_key: any, value: string) {
+    setItem( value: string) {
       return Promise.resolve(value);
     },
     removeItem() {
       return Promise.resolve();
     },
   };
-}; * /}
+};
 
-{/*const storage =
+const storage: WebStorage | Storage =
   typeof window === "undefined"
     ? createNoopStorage()
-    : createWebStorage("local"); */}
+    : createWebStorage("local");
 
-{/*const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["global"],
-};*/}
+// Define root reducer first
 const rootReducer = combineReducers({
   global: globalReducer,
   [api.reducerPath]: api.reducer,
 });
-{/*const persistedReducer = persistReducer(persistConfig, rootReducer);*/}
+
+// Define RootState type based on rootReducer
+export type RootState = ReturnType<typeof rootReducer>;
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["global"],
+};
+
+// Explicitly type the persisted reducer
+const persistedReducer = persistReducer<RootState>(persistConfig, rootReducer);
 
 /* REDUX STORE */
 export const makeStore = () => {
-  return configureStore({
-    reducer: rootReducer,
+  const store = configureStore({
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
-        serializableCheck: false,
-      }),
-  })
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }).concat(api.middleware),
+  });
+  return store;
 };
 
 /* REDUX TYPES */
 export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 /* PROVIDER */
-export default function StoreProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function StoreProvider({ children }: { children: React.ReactNode }) {
   const storeRef = useRef<AppStore | null>(null);
+
   if (!storeRef.current) {
     storeRef.current = makeStore();
     setupListeners(storeRef.current.dispatch);
   }
-  const persistor = persistStore(storeRef.current);
+
+  const persistor = useRef(persistStore(storeRef.current)).current;
 
   return (
     <Provider store={storeRef.current}>
