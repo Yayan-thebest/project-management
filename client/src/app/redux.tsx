6494855ac14b77cf1ba75b4bfa89ef00d15818c7
Yayan-thebest <monedgard@yahoo.com>
2025-02-23 15:47:56 -0500

    Correction ESLint et typage TypeScript

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -5,83 +5,83 @@ import {
   TypedUseSelectorHook,
   useDispatch,
   useSelector,
+  Provider,
 } from "react-redux";
-import { Provider } from "react-redux";
 import globalReducer from "@/state";
 import { api } from "@/state/api";
 import { setupListeners } from "@reduxjs/toolkit/query";
+
 import {
   persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
+
 } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
-import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
 /* REDUX PERSISTENCE */
-const createNoopStorage = () => ({
-  getItem: async () => null,
-  setItem: async (_key: string, value: string) => value,
-  removeItem: async () => {},
-});
+{/*const createNoopStorage = () => {
+  return {
+    getItem() {
+      return Promise.resolve(null);
+    },
+    setItem(_key: any, value: string) {
+      return Promise.resolve(value);
+    },
+    removeItem() {
+      return Promise.resolve();
+    },
+  };
+}; * /}
 
-const storage =
+{/*const storage =
   typeof window === "undefined"
     ? createNoopStorage()
-    : createWebStorage("local");
+    : createWebStorage("local"); */}
 
-const persistConfig = {
+{/*const persistConfig = {
   key: "root",
   storage,
   whitelist: ["global"],
-};
-
+};*/}
 const rootReducer = combineReducers({
   global: globalReducer,
   [api.reducerPath]: api.reducer,
 });
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+{/*const persistedReducer = persistReducer(persistConfig, rootReducer);*/}
 
 /* REDUX STORE */
-export const makeStore = () =>
-  configureStore({
-    reducer: persistedReducer,
+export const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }).concat(api.middleware),
-  });
+        serializableCheck: false,
+      }),
+  })
+};
 
 /* REDUX TYPES */
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 /* PROVIDER */
-export default function StoreProvider({ children }: { children: React.ReactNode }) {
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const storeRef = useRef<AppStore | null>(null);
-  
   if (!storeRef.current) {
     storeRef.current = makeStore();
     setupListeners(storeRef.current.dispatch);
   }
-
-  const persistorRef = useRef(persistStore(storeRef.current)).current;
+  const persistor = persistStore(storeRef.current);
 
   return (
     <Provider store={storeRef.current}>
-      <PersistGate loading={null} persistor={persistorRef}>
+      <PersistGate loading={null} persistor={persistor}>
         {children}
       </PersistGate>
     </Provider>
